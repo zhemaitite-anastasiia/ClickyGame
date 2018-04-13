a031ccc35a38713b1272da60d0a955879cb2bb90
AnastasiiaZhemaitite <a.zhemaitite@gmail.com>
2018-04-13 14:40:46 -0500

    Styling added to the rest of the components

--- clickygame/src/components/Rules/Rules.js
+++ clickygame/src/components/Rules/Rules.js
@@ -5,8 +5,7 @@ const Rules = props => (
 <div className="col-md-12">
 <div className="gameInstructions">
 <h1>Game Instructions:</h1>
-<h2>Click on the images but never clic kon the same one twice.</h2>
-<h3>Or you will lose!</h3>
+<h2>Click on the images but never click on the same one twice.</h2>
 </div>
 </div>
 );
