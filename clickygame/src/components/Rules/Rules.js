879e6c79070f5e92676cce580a1c7699a9f14362
AnastasiiaZhemaitite <a.zhemaitite@gmail.com>
2018-04-12 23:39:24 -0500

    Rules component created

---
+++ clickygame/src/components/Rules/Rules.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Rules.css";
+
+const Rules = props => (
+<div className="col-md-12">
+<div className="gameInstructions">
+<h1>Game Instructions:</h1>
+<h2>Click on the images but never clic kon the same one twice.</h2>
+<h3>Or you will lose!</h3>
+</div>
+</div>
+);
+
+export default Rules;
