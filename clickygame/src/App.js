9d1b5333090d7469f6fc5c636765984fa3a48d51
AnastasiiaZhemaitite <a.zhemaitite@gmail.com>
2018-04-13 13:57:15 -0500

    App logic added

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,42 +1,102 @@
 import React, { Component } from 'react';
+import Cards from "./components/Cards";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import Rules from "./components/Rules";
+import images from "./images.json";
+import "./App.css";
 
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-<div>
-<nav className="nav">
-  <a className="nav-link disabled" >Clicky Game </a>
-  <a className="nav-link disabled" >Click an image to begin! </a>
-  <a className="nav-link disabled" >Score: 0 | Top Score: 0</a>
-</nav>
-<div className="container-fluid">
-<h1>Clicky Game!</h1>
+  state= {
+    score: 0,
+    highScore: 0,
+    images: images
+  };
 
-<h2>Click on an image to earn points, but don't click on any more than once!</h2>
-</div>
-<main class="container">
- <img alt="" src={"https://cdn.pastemagazine.com/www/blogs/lists/2010/05/13/homer_simpson_300.jpg"} />
-<img alt="" src={'https://img00.deviantart.net/282b/i/2007/002/4/3/spongebob_has_hair__by_deviantretard.jpg'} />
-<img alt="" src={'http://cdn.smosh.com/sites/default/files/2014/11/cartoon-heads-stewie.jpg'} />
-<img alt="" src={'https://www.whudat.de/images/Big_Heads_Illustrated_Characters_From_Famous_Movies-_and_TV_Series_2014_05.jpg'} />
-<img alt="" src={'http://www.landaasmenighet.no/Portals/132/3602836742_6f8c876e28[1].jpg'} />
-<img alt="" src={'https://i.pinimg.com/originals/70/aa/c0/70aac08890249dfa09f4f3cd3de182f2.png'} />
-<img alt="" src={'https://vignette.wikia.nocookie.net/star-and-the-forces-of-evil/images/7/7f/Pony_Head_-_Goblin_Dogs_profile.png/revision/latest?cb=20170225163148'} />
-<img alt="" src={'https://lumiere-a.akamaihd.net/v1/images/9428da2563387bf08c4b2ba32af5db03169fa0d5.jpeg'} />
-<img alt="" src={'https://i.pinimg.com/originals/d3/d9/0b/d3d90bc572972bec3d0f6f58e4f8795a.jpg'} />
-<img alt="" src={'https://g7d8r29h4o12bn43l2xflqq5-wpengine.netdna-ssl.com/wp-content/uploads/2016/05/vectoredit_sectionheads_characters2.png'} />
-</main>
-<footer className="footer">
-"ClickyGame!"
-<img alt= "react" style={{width:'40px', height:"30px", }} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" />
-  </footer>
-</div>
+  randomRender = id => {
+    this.state.images.forEach((image) => {
+      if (image.id === id) {
+        if (image.clicked) {
+          alert("You Lost!! This card was previously selected.");
+          this.setState({})
+          this.resetGame();
+          return false;
+        }
+        else {
+          this.updateScore();
+          image.clicked =true;
+        }
+        if(this.state.score >= this.state.highScore){
+          this.newHighScore();
+        }
+        }
+        });
+    }
+
+    randomOrganize = (array) => {
+     let copy = [], n = array.length, i;
+     while (n) {
+       i = Math.floor(Math.random() * array.length);
+       if (i in array) {
+         copy.push(array[i]);
+         delete array[i];
+         n--;
+       }
+     }
+     this.setState({ images: copy });
+
+    }
+
+    updateScore = () => {
+      this.setState((newState) => ({
+        score: newState.score + 1
+
+      }), () => this.winning())
+    }
+
+    newHighScore = () => {
+      this.setState((newState) => ({
+        highScore: newState.score
+      }))
+    }
+    wining = () => {
+      if (this.state.score === this.state.images) {
+        alert("YOU WIN! Congratulations!")
+        this.setState({});
+        this.resetGame();
+      }
+      else {
+        setTimeout (() => {
+          this.randomOrganize(this.state.images)
+        }, 500);
+      }
+    }
+    resetGame = () => {
+      this.state.images.forEach((image) => {
+        image.clicked =false;
+      })
+      this.setState({ score:0 })
+    }
+
+    //Map over this.state.images and render Cards component for each friend object
+
+   render() {
+     return (
+       <Wrapper>
+         <Header score={this.state.score} highScore={this.state.highScore} />
+         <Rules />
+         {this.state.images.map(image => {
+           return < Cards
+          {...image}
+          key={image.id}
+         randomRender={this.randomRender}
+        randomOrganize={() => this.randomOrganize(this.state.images)} />;
+         })}
+         </Wrapper>
+     )};   
+   }
 
 
-   )
-  }
- }
+   export default App;
 
-export default App;
