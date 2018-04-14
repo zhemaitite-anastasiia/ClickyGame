954d3d3a1c8a5ea919df90ceedf17e659c2ebd0c
AnastasiiaZhemaitite <a.zhemaitite@gmail.com>
2018-04-13 21:41:18 -0500

    final changes

--- clickygame/src/components/Header/Header.js
+++ clickygame/src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = props => (
     <nav className="navbar navbar-default">
         <ul>
         <li className = "brand">Clicky Game</li>
-        <li>Click on an image to start!</li>
+        <li>Click on the image to start!</li>
         <li>Current Score: {props.score} | Highest Score: {props.highScore} </li>
         </ul>
     </nav>
